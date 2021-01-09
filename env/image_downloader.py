03af66ca1bebee670ff7ec40ec4764a8e4a4a2a3
Colin Szeto <kolinszeto@gmail.com>
2021-01-08 16:52:02 -0800

    created a file to scrape all images from the provided website

---
+++ env/image_downloader.py
@@ -0,0 +1,39 @@
+# the intent of this file is to download all images displayed on amazon
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+def imagedown(url, folder):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), folder))  # creating a folder in the main directory
+    except:
+        pass  # continues on the program if creation failed, will be pasting all of the images into
+
+    parent_dir = "D:\IntellijProjects\P5-Tacos-thrift-store\env\static\images"  #  this is the directory that the static images are at
+    path = os.path.join(parent_dir, folder)
+    os.mkdir(path)
+    #  os.chdir(os.path.join(os.getcwd(), folder))  # creating a folder based off of name provided
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')  # looking for specific tag
+    images = soup.find_all('img')  # looking for all img tags
+    x = 50
+    for image in images:
+        name = 'item' + str(x)  # naming the file item1
+        link = image['src']  # getting the formatting with the src tag
+        with open(name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:
+            im = requests.get(link)
+            f.write(im.content)
+            print('Writing: ', name)  # communicating back the the user
+        x = x + 1
+
+
+print("This is the image downloader")
+url = 'https://www.etsy.com/market/vintage_t_shirt'
+
+folder_name = 'T-Shirts'
+print('this is the link we are scraping from ' + url)
+print('This is the folder we will be creating ' + folder_name)
+imagedown(url, folder_name)
--- env/thriftythreadsdata.py
+++ env/thriftythreadsdata.py
@@ -1,40 +1,40 @@
 def TT1():
     name = "boxing shorts"
-    tag ="shorts"
+    tag = "shorts"
     description = "for lighting fast feet"
     size = "Mens Large"
     price = "30"
-    folder ="thrifty_threads" #name of folder file is stored in
+    folder = "thrifty_threads"  #name of folder file is stored in
     info = {"name": name,"tag":tag, "description": description, "size":size, "price": price, "folder": folder}
     return info
 
 def TT2():
     name = "blue polo"
-    tag ="shirt"
+    tag = "shirt"
     description = "great for any look"
     price = "10"
     size = "Female Large"
-    folder ="thrifty_threads" #name of folder file is stored in
+    folder = "thrifty_threads" #name of folder file is stored in
     info = {"name": name,"tag":tag, "description": description, "size":size, "price": price, "folder": folder}
     return info
 
 def TT3():
     name = "Fo Leather Jacket"
-    tag ="jacket"
+    tag = "jacket"
     description = "Looks like leather"
     price = "30"
     size = "Mens medium"
-    folder ="thrifty_threads" #name of folder file is stored in
+    folder = "thrifty_threads" #name of folder file is stored in
     info = {"name": name,"tag":tag, "description": description, "size":size, "price": price, "folder": folder}
     return info
 
 def TT4():
     name = "sandals"
-    tag ="footwear"
+    tag = "footwear"
     description = "studded with jewels"
     price = "10"
     size = "kids 10"
-    folder ="thrifty_threads" #name of folder file is stored in
+    folder = "thrifty_threads" #name of folder file is stored in
     info = {"name": name,"tag":tag, "description": description, "size":size, "price": price, "folder": folder}
     return info
 

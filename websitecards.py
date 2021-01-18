c0a2391b87b7ac678e5544e520f2fb16c5400a7f
Colin Szeto <kolinszeto@gmail.com>
2021-01-17 20:51:00 -0800

    changed the url in the websitecards.py to work with all hosted ip addresses

--- websitecards.py
+++ websitecards.py
@@ -1,7 +1,7 @@
 def WW1():
     info = {
     "websitename" : "Thrifty Threads",
-    "hyperlinktopage": "http://127.0.0.1:5000/thriftythreads",
+    "hyperlinktopage": "/thriftythreads",
     "PictureShop" : "../static/images/storefront/thriftythreadsimage.jpg",
     "Location" : "607 S Coast Hwy 101, Encinitas, CA 92024",
     "linklocation" : "https://www.google.com/maps/place/Thrifty+Threads/@33.0459426,-117.2963977,16z/data=!4m13!1m7!3m6!1s0x80dc0c423138b543:0xebb9278c8422a06f!2sThrifty+Threads!8m2!3d33.0445576!4d-117.2931898!10e1!3m4!1s0x80dc0c423138b543:0xebb9278c8422a06f!8m2!3d33.0445576!4d-117.2931898"
@@ -11,7 +11,7 @@ def WW1():
 def WW2():
     info = {
     "websitename" : "Barbarella",
-    "hyperlinktopage": "http://127.0.0.1:5000/barbarella",
+    "hyperlinktopage": "/barbarella",
     "PictureShop" : "../static/images/storefront/barbarellaimage.jpg",
     "Location" : "570 S Coast Hwy 101, Encinitas, CA 92024",
     "linklocation" : "https://www.google.com/maps/place/570+S+Coast+Hwy+101,+Encinitas,+CA+92024/@33.0450411,-117.296177,17z/data=!3m1!4b1!4m5!3m4!1s0x80dc0c424dc26f81:0xbaffd7719c53424d!8m2!3d33.045036!4d-117.293988"

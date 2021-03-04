ad073805eb285399ea30559103ab41c3f9b93b54
Colin Szeto <kolinszeto@gmail.com>
2021-03-04 13:32:18 -0800

    added in runner database and pages for the runners

--- models/module.py
+++ models/module.py
@@ -84,5 +84,17 @@ class userEE(db.Model):
         self.password = password
     pass
 
+class userRR(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+    pass
+
 "Create Database"
 db.create_all()
